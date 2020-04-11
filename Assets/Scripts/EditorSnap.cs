76d1feaf0a7a0feb4d0590b10dc2e366bcacf96f
BorisBBoychev <borisboychev007@Gmail.com>
2020-04-11 22:59:39 +0300

    Initial commit

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Range(1f,20f)]float snapParZ = 10f;
+    [SerializeField] [Range(1f, 20f)] float snapParX = 10f;
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / snapParX) * snapParX;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / snapParZ) * snapParZ;
+        transform.position = new Vector3(snapPos.x , 0 , snapPos.z);
+
+    }
+}
