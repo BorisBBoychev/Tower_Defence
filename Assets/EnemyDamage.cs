f0bdd1007cb1875a9abf7272fb2b274f7878b758
BorisBBoychev <borisboychev007@Gmail.com>
2020-04-12 22:21:43 +0300

    enemy particles and collision

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] private int hitPoints = 10;
+
+    void Start()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    void ProcessHit()
+    {
+        hitPoints -= 1;
+    }
+}
